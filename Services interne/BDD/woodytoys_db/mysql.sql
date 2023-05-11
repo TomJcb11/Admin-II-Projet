657e1a77537c6589eb11ea198a226ede08ad6ff1
Louis Delbrouck <louisdelbrouck@gmail.com>
2023-05-11 03:54:51 +0200

    public

--- Services interne/BDD/woodytoys_db/mysql.sql
+++ Services interne/BDD/woodytoys_db/mysql.sql
@@ -10,6 +10,6 @@ INSERT INTO `woodytoys_db`.`articles`
 (`nom`,
 `prix`)
 VALUES
-("table", 40),("chaise", 20),("cheval a bascule",150),("table de chevet",25),("armoire",45),("lit",100);
+("table", 40),("chaise", 20),("cheval a bascule",150),("table de chevet",25),("armoire",45),("lit",100),("bureau",125);
 
 GRANT ALL PRIVILEGES ON woodytoys_db.* TO 'admin';
